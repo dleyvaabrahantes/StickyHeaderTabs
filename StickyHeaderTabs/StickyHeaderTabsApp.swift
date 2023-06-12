f9ff22720aef845089ca4a87e9414bc523c22c73
David Leyva <dleyvaabrahantes@gmail.com>
2023-06-12 16:40:12 -0400

    Initial Commit

---
+++ StickyHeaderTabs/StickyHeaderTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderTabsApp.swift
+//  StickyHeaderTabs
+//
+//  Created by David on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
